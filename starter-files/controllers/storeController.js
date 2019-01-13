90a79d1157418aaffb482c57be276ea6ec54bc18
Brittany Chiang <brittany.chiang@gmail.com>
2019-01-13 17:16:27 -0500

    Add ups linting configs

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,11 +1,11 @@
 // middleware allows you run thins _after_ the request but _before_ the response
 exports.myMiddleware = (req, res, next) => {
-	req.name = 'Brit';
-	res.cookie('name', 'Brittany is cool', { maxAge: 7000000 });
-	next();
-}
+  req.name = 'Brit';
+  res.cookie('name', 'Brittany is cool', { maxAge: 7000000 });
+  next();
+};
 
 exports.homePage = (req, res) => {
-	console.log(req.name);
-	res.render('index');
-}
+  // console.log(req.name);
+  res.render('index');
+};
