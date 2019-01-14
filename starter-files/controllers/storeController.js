9904ed08522a188b96ff8ce11d4308091f9f5ca4
Brittany Chiang <brittany.chiang@gmail.com>
2019-01-13 21:13:28 -0500

    Module 3

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -6,6 +6,13 @@ exports.myMiddleware = (req, res, next) => {
 };
 
 exports.homePage = (req, res) => {
-  // console.log(req.name);
   res.render('index');
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
 };
