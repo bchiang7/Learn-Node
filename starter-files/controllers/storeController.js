80ba59416aa5520b963acdcfd58625201225510c
Brittany Chiang <brittany.chiang@gmail.com>
2019-01-13 23:35:03 -0500

    Module 4

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 // middleware allows you run thins _after_ the request but _before_ the response
 exports.myMiddleware = (req, res, next) => {
   req.name = 'Brit';
@@ -13,6 +16,37 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // 1. Query the DB for a list of all stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. confirm they are the owner of the store
+  // TODO
+  // 3. Render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true,
+  }).exec();
+  req.flash(
+    'success',
+    `Successfully updated ${store.name}. <a href="/stores/${store.slug}">View Store</a>`,
+  );
+  // redirect them to the store and tell them it worked
+  res.redirect(`/stores/${store._id}/edit`);
 };
