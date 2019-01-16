29450b32b1fd1bbfeec6fe9ead71653060432c21
Brittany Chiang <brittany.chiang@gmail.com>
2019-01-15 23:39:11 -0500

    Module 6

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: `That filetype isn't allowed!` }, false);
+    }
+  },
+};
 
 // middleware allows you run thins _after_ the request but _before_ the response
 exports.myMiddleware = (req, res, next) => {
@@ -14,6 +29,24 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  // check if there is no new file to resize
+  if (!req.file) {
+    next(); // skip to next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // resize the photo
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  // once we have written the photo to the fs, keep going
+  next();
 };
 
 exports.createStore = async (req, res) => {
@@ -51,4 +84,14 @@ exports.updateStore = async (req, res) => {
   );
   // redirect them to the store and tell them it worked
   res.redirect(`/stores/${store._id}/edit`);
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+
+  if (!store) {
+    return next();
+  }
+
+  res.render('store', { store, title: store.name });
 };
